b06f199eb55362ab21f64e08e73ec86439c766e2
elfzweik <wzxuan@foxmail.com>
2020-11-12 00:51:04 +0800

    blogsite

--- blogsite/settings/base.py
+++ blogsite/settings/base.py
@@ -164,7 +164,7 @@ STATICFILES_DIRS = [
 # See https://docs.djangoproject.com/en/3.1/ref/contrib/staticfiles/#manifeststaticfilesstorage
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_collected')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
