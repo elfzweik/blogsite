aeb0fd10ac962bc80896b790a4fcaaf325126d8d
elfzweik <wzxuan@foxmail.com>
2020-11-15 03:17:30 +0800

    soring by revision date

--- blog/models.py
+++ blog/models.py
@@ -40,8 +40,8 @@ class BlogListingPage(Page):
 
     def get_context (self, request, *args, **kwargs):
         context=super().get_context(request, *args, **kwargs)
-        blogpages = BlogDetailPage.objects.live().public().order_by('-first_published_at')
-        latest_blogs= blogpages[:3]
+        blogpages = BlogDetailPage.objects.live().public().order_by('latest_revision_created_at')
+        latest_blogs= blogpages[:5]
         paginator = Paginator(blogpages, 12)
         page_num = request.GET.get('page', 1)
         try:
@@ -87,7 +87,7 @@ class BlogTagIndexPage(Page):
 
         # Filter by tag
         tag = request.GET.get('tag')
-        blogpages = BlogDetailPage.objects.filter(tags__name=tag)
+        blogpages = BlogDetailPage.objects.filter(tags__name=tag).order_by('latest_revision_created_at')
         paginator = Paginator(blogpages, 12)
         page_num = request.GET.get('page', 1)
         try:
