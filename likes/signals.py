ec7f5c5581f23eeb95adf3a4fd22eaeb4f2b68c6
elfzweik <wzxuan@foxmail.com>
2022-05-04 19:33:34 -0400

    update

--- likes/signals.py
+++ likes/signals.py
@@ -9,7 +9,7 @@ from .models import LikeRecord
 def send_notification(sender, instance, **kwargs):
     if instance.content_type.model == 'blogdetailpage':
         blog = instance.content_object
-        verb = '点赞了你的《{0}》' 
+        verb = '点赞了你的《{0}》'.format(blog.title)
         #.format( blog.title)
     elif instance.content_type.model == 'mycomment':
         comment = instance.content_object
