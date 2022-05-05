7b8feddc6aea8fb7d52a0bccb16578a95e4426b5
elfzweik <wzxuan@foxmail.com>
2022-05-04 20:35:07 -0400

    update

--- blogsite/settings/base.py
+++ blogsite/settings/base.py
@@ -99,7 +99,7 @@ WSGI_APPLICATION = 'blogsite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -115,9 +115,17 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    },
+    'slave': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blogsite_db',
+        'USER': 'elf',
+        'PASSWORD': 'Ig2rdf@oyx',
+        'HOST': 'localhost',
+        'PORT': '5432' ,
+    },
 }
-'''
+
 """
 WAGTAILSEARCH_BACKENDS = {
     'default': {
