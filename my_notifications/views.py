ebfe6ae988e5266c5fa49375d33dcb6ad1e33a23
elfzweik <wzxuan@foxmail.com>
2022-05-14 00:09:23 -0400

    update

--- my_notifications/views.py
+++ my_notifications/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from notifications.models import Notification
-
+from blog.models import BlogDetailPage
+from visitor_record.utils import count_visits
 
 def my_notifications(request):
+    obj = BlogDetailPage.objects.all()[0]
+    data = count_visits(request, obj)
+
     context = {}
-    return render(request, 'my_notifications/my_notifications.html', context)
+    context['client_ip'] = data['client_ip']
+    context['location'] = data['location']
+    context['total_hits'] = data['total_hits']
+    context['total_visitors'] =data['total_vistors']
+    context['cookie'] = data['cookie']
+    response = render(request, 'my_notifications/my_notifications.html', context)
+    response.set_cookie(context['cookie'], 'true', max_age=300)
+    return response
     
 def my_notification(request, my_notification_pk):
     my_notification = get_object_or_404(Notification, pk=my_notification_pk)
