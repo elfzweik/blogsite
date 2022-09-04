d2bcf77b84ed8325cf8962d5afcecf217ae6ad7a
elfzweik <wzxuan@foxmail.com>
2022-09-03 22:18:48 -0400

    update

--- blogsite/settings/production.py
+++ blogsite/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = False
 
-CSRF_TRUSTED_ORIGINS = ['https://blog.petal2collect.art','https://*.127.0.0.1','https://www.petal2collect.tk']
+CSRF_TRUSTED_ORIGINS = ['https://blog.petal2collect.ml','https://*.127.0.0.1','https://www.petal2collect.ga', 'https://www.wangfamily.ga']
 
 try:
     from .local import *
